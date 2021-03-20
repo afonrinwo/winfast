1035a0ecb01b7f8506383c385fe9a9e8fc27794e
Emmanuel Afonrinwo <Emmanuel Afonrinwo@DESKTOP-M4F66S7>
2021-03-20 21:03:49 +0100

    1-20032021

---
+++ winwallet/src/main/java/com/winwallet/WinwalletApplication.java
@@ -0,0 +1,13 @@
+package com.winwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinwalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinwalletApplication.class, args);
+	}
+
+}
